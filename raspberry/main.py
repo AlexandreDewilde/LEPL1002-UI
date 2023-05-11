6b821f90a3994e85eab3e1f731a995742f511f1f
allEyezOnCode <alexandredewilde2001@gmail.com>
2023-05-11 08:50:46 +0200

    update rasp script

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,13 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
+import requests
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.IN)
 last = None
 while True:
 	if GPIO.input(17) == 1:
-                current = time.time()
-                if last != None and 0.1 < (current-last) < 2: 
-                        print("Delta : {}".format((current-last)))
+		current = time.time()
+		if last != None and 0.1 < (current-last) < 4: 
+			print("Delta : {}".format((current-last)))
+			requests.post("https://moussimousse.louvainlinux.org/add_record", json={"time": int(datetime.datetime.now().timestamp()), "speed": 1 / (current - last)})
 			last = None
 		else:                      
 			last = current
